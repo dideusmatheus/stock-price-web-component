4a62eb931947d42b9ecbb067a561a887312b84df
Matheus de Deus Rocha <matheus.rocha@sis-it.com>
2025-01-20 14:55:11 -0300

    Making a call to the api and showing the value of the entered stock price

--- src/components/stock-price/stock-price.tsx
+++ src/components/stock-price/stock-price.tsx
@@ -1,25 +1,46 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, h , State} from '@stencil/core';
 
+import { AV_API_KEY } from '../../global/global';
 @Component({
   tag: 'md-stock-price',
   styleUrl: 'stock-price.css',
   shadow: true,
 })
 export class StockPrice {
+  stockInput : HTMLInputElement;
+
+  @State() objResponse: any = null; // escuta as alterações dinamicamente
+  @State()
 
   onFetchStockPrice(event: Event){
     event.preventDefault();
-    console.log('submitted!')
+
+    const stockSymbol = this.stockInput.value;
+
+    fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stockSymbol}&apikey=${AV_API_KEY}`)
+    .then( res => {
+      return res.json()
+    })
+    .then( parsedREs => {
+      this.objResponse = parsedREs;
+      console.log(this.objResponse);
+    })
+    .catch(error => {
+      console.log('error: ', error);
+    });
   }
 
   render() {
+    const price = this.objResponse?.['Global Quote']?.['05. price'] ?? 'N/A';
+    const symbol = this.objResponse?.['Global Quote']?.['01. symbol'] ?? 'N/A';
     return [
-      <form onSubmit={this.onFetchStockPrice}>
-        <input id="stock-synbol" type="text" />
+      <form onSubmit={this.onFetchStockPrice.bind(this)}>
+        <input id="stock-symbol" type="text" ref={el => this.stockInput = el}/>
         <button type='submit'> Fetch </button>
       </form>,
       <div>
-        <p> Price: { 0 } </p>
+        <p> Symbol: { symbol } </p>
+        <p> Price: ${ price } </p>
       </div>
     ];
   }
