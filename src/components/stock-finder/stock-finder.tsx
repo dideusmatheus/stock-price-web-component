12e8397594423bc3e731cbb2eb38b8d3d24db924
Matheus de Deus Rocha <matheus.rocha@sis-it.com>
2025-01-22 10:43:12 -0300

    Adjustment in api error logic

--- src/components/stock-finder/stock-finder.tsx
+++ src/components/stock-finder/stock-finder.tsx
@@ -29,122 +29,11 @@ export class StockFinder {
       }
 
       const parsedRes = await res.json();
+      if (!parsedRes['bestMatches'] || Object.keys(parsedRes['bestMatches']).length === 0) {
+        throw new Error('No results found.');
+      }
 
       this.searchResponse = parsedRes['bestMatches'];
-      // this.searchResponse = 
-      //   [
-      //     {
-      //       "1. symbol": "BA",
-      //       "2. name": "Boeing Company",
-      //       "3. type": "Equity",
-      //       "4. region": "United States",
-      //       "5. marketOpen": "09:30",
-      //       "6. marketClose": "16:00",
-      //       "7. timezone": "UTC-04",
-      //       "8. currency": "USD",
-      //       "9. matchScore": "1.0000"
-      //     },
-      //     {
-      //       "1. symbol": "BA.LON",
-      //       "2. name": "BAE Systems plc",
-      //       "3. type": "Equity",
-      //       "4. region": "United Kingdom",
-      //       "5. marketOpen": "08:00",
-      //       "6. marketClose": "16:30",
-      //       "7. timezone": "UTC+01",
-      //       "8. currency": "GBX",
-      //       "9. matchScore": "0.6667"
-      //     },
-      //     {
-      //       "1. symbol": "BA05.LON",
-      //       "2. name": "BA05",
-      //       "3. type": "Equity",
-      //       "4. region": "United Kingdom",
-      //       "5. marketOpen": "08:00",
-      //       "6. marketClose": "16:30",
-      //       "7. timezone": "UTC+01",
-      //       "8. currency": "GBP",
-      //       "9. matchScore": "0.6667"
-      //     },
-      //     {
-      //       "1. symbol": "BA29.LON",
-      //       "2. name": "BA29",
-      //       "3. type": "Equity",
-      //       "4. region": "United Kingdom",
-      //       "5. marketOpen": "08:00",
-      //       "6. marketClose": "16:30",
-      //       "7. timezone": "UTC+01",
-      //       "8. currency": "GBP",
-      //       "9. matchScore": "0.6667"
-      //     },
-      //     {
-      //       "1. symbol": "BA69.LON",
-      //       "2. name": "BA69",
-      //       "3. type": "Equity",
-      //       "4. region": "United Kingdom",
-      //       "5. marketOpen": "08:00",
-      //       "6. marketClose": "16:30",
-      //       "7. timezone": "UTC+01",
-      //       "8. currency": "GBP",
-      //       "9. matchScore": "0.6667"
-      //     },
-      //     {
-      //       "1. symbol": "BA3.FRK",
-      //       "2. name": "Brooks Automation Inc",
-      //       "3. type": "Equity",
-      //       "4. region": "Frankfurt",
-      //       "5. marketOpen": "08:00",
-      //       "6. marketClose": "20:00",
-      //       "7. timezone": "UTC+02",
-      //       "8. currency": "EUR",
-      //       "9. matchScore": "0.5714"
-      //     },
-      //     {
-      //       "1. symbol": "BAAPL",
-      //       "2. name": "null",
-      //       "3. type": "Equity",
-      //       "4. region": "United States",
-      //       "5. marketOpen": "09:30",
-      //       "6. marketClose": "16:00",
-      //       "7. timezone": "UTC-04",
-      //       "8. currency": "USD",
-      //       "9. matchScore": "0.5714"
-      //     },
-      //     {
-      //       "1. symbol": "BAAPV",
-      //       "2. name": "null",
-      //       "3. type": "Equity",
-      //       "4. region": "United States",
-      //       "5. marketOpen": "09:30",
-      //       "6. marketClose": "16:00",
-      //       "7. timezone": "UTC-04",
-      //       "8. currency": "USD",
-      //       "9. matchScore": "0.5714"
-      //     },
-      //     {
-      //       "1. symbol": "BAAAAX",
-      //       "2. name": "Building America Strategy Port CDA USD Ser 21/1Q MNT CASH",
-      //       "3. type": "Mutual Fund",
-      //       "4. region": "United States",
-      //       "5. marketOpen": "09:30",
-      //       "6. marketClose": "16:00",
-      //       "7. timezone": "UTC-04",
-      //       "8. currency": "USD",
-      //       "9. matchScore": "0.5000"
-      //     },
-      //     {
-      //       "1. symbol": "BAAAFX",
-      //       "2. name": "Building America Strgy Portf CDA USD Ser 2022/2Q MNT CASH",
-      //       "3. type": "Mutual Fund",
-      //       "4. region": "United States",
-      //       "5. marketOpen": "09:30",
-      //       "6. marketClose": "16:00",
-      //       "7. timezone": "UTC-04",
-      //       "8. currency": "USD",
-      //       "9. matchScore": "0.5000"
-      //     }
-      //   ];
-      console.log('objResponse', this.searchResponse)
     } catch (err) {
       this.error = err.message;
       this.searchResponse = null;
@@ -160,10 +49,16 @@ export class StockFinder {
     this.mdsymbolSelected.emit(stockSymbol);
   }
 
+  clearSearchResponse(){
+    if (this.stockNameInput.value.trim() === '') {
+      this.searchResponse = null;
+    }
+  }
+
   render() {
     return [
       <form onSubmit={this.onFindStocks.bind(this)}>
-        <input id="stock-symbol" ref={(el) => (this.stockNameInput = el)}  placeholder="Stock Finder"/>
+        <input id="stock-symbol" ref={(el) => (this.stockNameInput = el)} onInput={this.clearSearchResponse.bind(this)} placeholder="Stock Finder"/>
         <button type="submit">Fetch</button>
       </form>,
       <div class="info">
@@ -181,7 +76,7 @@ export class StockFinder {
             </div>
           ))
         ) : (
-          <p class="no-results"> {this.error ? this.error : 'No results found'} </p>
+          this.error && <p class="no-results">{this.error}</p>
         )}
       </div>
     ];
